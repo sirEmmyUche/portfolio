8f6ed0ed0b1e4fc4b1f7f7a9b02e5b43576162d0
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2023-08-10 17:18:59 +0100

    changing layout structure

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import Intro from "../pages/intro"
+import Nav from "../pages/nav"
 
 
  function Home(){
     return(
             <header className="home-header">
+                 <Nav/>
                 <Intro/>
             </header>
     )
